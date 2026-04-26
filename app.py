d2d894b5b461e66d5f7e96191f32d6e3f0a2bb0a
nevlock59-langlock <nevlock59@gmail.com>
2026-04-26 21:24:16 +0900

    Update button location
    Update button location

--- app.py
+++ app.py
@@ -1204,7 +1204,7 @@ if __name__ == "__main__":
     # 4. 왼쪽 단: 상품 선택 + 대상기간 선택
     with metric_col:
         with st.container(key="asset_title_reset_group"):
-            asset_title_col, asset_reset_col = st.columns([0.9, 0.1], gap="small")
+            asset_title_col, asset_reset_col = st.columns([0.95, 0.05], gap="small")
     
             with asset_title_col:
                 st.markdown(
